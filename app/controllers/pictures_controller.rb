15fd84ac3a0d2a51be3ed45cb08a18f6ed5b1f10
Pooja Parthasarathy <ppartha0@chicagobooth.edu>
2017-07-20 00:05:43 +0000

    starting rcav dots

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,12 @@
+class PicturesController < ApplicationController
+    
+    def new_form
+        
+        render("pic_templates/new_form.html.erb")
+    end
+    
+    def create_row
+    end
+    
+    
+end
